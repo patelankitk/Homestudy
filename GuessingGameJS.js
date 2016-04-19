0b457acbec268887939294396bc25c9e073f304d
patelankitk <patelankitk@yahoo.com>
2016-04-19 20:28:41 +0100

    Change

--- GuessingGameJS.js
+++ GuessingGameJS.js
@@ -3,12 +3,7 @@
 var x = Math.floor((Math.random() * 100) + 1);
     
 // Creating a variable for number of guess
-var guess =0;
-
-var event1 = document.getElementById("button");
-
-// invoking an even listener.
-event1=addEventListener('click',randomvalue,false);
+var guess = 0;
 
 
 
@@ -21,41 +16,38 @@ event1=addEventListener('click',randomvalue,false);
 
             if (x < y)
                 {
+                    guess++;
                     document.getElementById("demo").innerHTML="The number is too high."
                     var y = parseInt(document.getElementById("userinput").value);
-                    guess++;
-                    
 
                 }
             else if (x > y)
                 {
-                    document.getElementById("demo").innerHTML="The number is too low."
-                    var y = parseInt(document.getElementById("userinput").value);
                     guess++;
-                    
 
+                    document.getElementById("demo").innerHTML="The number is too low."
+                    var y = parseInt(document.getElementById("userinput").value);                   
+                    
                 }
         
-            else if(x===y)
+            else if (x===y)
                 {
                     guess++;
                     document.getElementById("demo").innerHTML="Well Done you guess the correct number."+"<br>" + "The correct Number was " + x +".<br>"+ "Total No of Guess:- "+ guess;
-                    guess=0;
-                   
-                    
-                    
+       
+                                        
                 }
+
            
     }
-var event2 = document.getElementById("play");
 
-        // invoking an even listener.
-event2=addEventListener('click',play,false);
-    function play()
-    {
-        reload(false)
+function play()
+{
+    window.location.reload();
                
-    }
+}
+
+
 
 
 
