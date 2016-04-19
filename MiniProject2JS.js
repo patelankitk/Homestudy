994edc1a56ee308f0ee116812649d5e26d140fa4
patelankitk <patelankitk@yahoo.com>
2016-04-19 21:58:48 +0100

    change

--- MiniProject2JS.js
+++ MiniProject2JS.js
@@ -1,28 +1,19 @@
-var event1 = document.getElementById("button");
-
-// invoking an even listener.
-event1=addEventListener('click',interestcal,false);
-
 
 
     function interestcal()
     {
-        var amount =parseInt(document.getElementById("amount").value);
-        var interest =parseInt(document.getElementById("interest").value);        
-        var duration =parseInt(document.getElementById("duration").value);
-        var month= (duration*12);
-        
-        
    
+        var amount =parseInt(document.getElementById("amount").value);
+        var interest_rate =parseInt(document.getElementById("interest").value);  
+        var interest = interest_rate / 100;
+        var months =parseInt(document.getElementById("month").value);
+        var year = (months/12).toFixed(2);
         
-	var interest = (amount * (interest * .01)) / month;
-	var payment = ((x / month) + interest).toFixed(2);
-	
-
         
-
-
-
+        var totalpayment = amount*(1+(interest * year));
+        var monthlypayment = totalpayment/12;
+        var totalinterestpayable = (amount*interest*year);
+        document.getElementById('payment').innerHTML = "Monthly Payment = "+monthlypayment+"<br>"+ "Total Payment = "+totalpayment+"<br>"+"Total Interest Payable = "+totalinterestpayable;
     }
 
 
